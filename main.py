a8df44a1d41fe583f6c71543dc43665a457fcdcb
Abdulmajeed Alsuwaidan <136227316+Bazinga1026@users.noreply.github.com>
2026-01-29 14:09:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ a_hat = squarePrice_mean - (b_hat * pAge_mean)
 for test_age in range(0, 31, 5):
     test_price = price_predictor(a_hat, b_hat, test_age)
     test_price = round(test_price, 2)
-    print(f"Property age = {test_age} | price = {test_price} Riyals/square meter")
+    print(f"Property age = {test_age} | price = {test_price} Riyals/Square Meter")
